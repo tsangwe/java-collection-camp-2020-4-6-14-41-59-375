8d7113b05ff17d5ede7ab669896413ffcf12c9d3
Wesley Tsang <tsangwe@oocl.com>
2020-04-07 11:18:52 +0800

    Complete getSumOfEvens

--- src/main/java/com/thoughtworks/collection/Add.java
+++ src/main/java/com/thoughtworks/collection/Add.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public class Add {
     public int getSumOfProcessedOdds(List<Integer> arrayList) {
+
         throw new NotImplementedException();
     }
 
@@ -27,7 +28,22 @@ public class Add {
     }
 
     public int getSumOfEvens(int leftBorder, int rightBorder) {
-        throw new NotImplementedException();
+        int sumOfEvens = 0;
+        int start = leftBorder;
+        int end = rightBorder;
+        
+        if (leftBorder > rightBorder) {
+            start = rightBorder;
+            end = leftBorder ;
+        }
+        if (start%2 != 0) {
+            start++;
+        }
+        for (int currNumber = start; currNumber <= end; currNumber+=2) {
+            sumOfEvens += currNumber;
+        }
+        
+        return sumOfEvens;
     }
 
     public int getSumOfOdds(int leftBorder, int rightBorder) {
